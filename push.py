6f0386781fb35043eb7c059302385f3af9d303ef
ikajakam <158202994+ikajakam@users.noreply.github.com>
2025-12-11 08:21:25 +0530

    Update push.py

--- push.py
+++ push.py
@@ -30,7 +30,7 @@ def main():
     # This proves they are valid without needing Scapy.
     hash_files = glob.glob(os.path.join(HANDSHAKE_DIR, "*.22000"))
     
-    print(f"Found {len(hash_files)} valid handshakes. 💋 💋 💋 jizzing 💋 💋 💋 ")
+    print(f"Found {len(hash_files)} valid handshakes 💋 💋 💋 jizzing 💋 💋 💋 ")
 
     uploaded_count = 0
     
